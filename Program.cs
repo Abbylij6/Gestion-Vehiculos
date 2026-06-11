9051f7c3c5bb3db3af3256766499b6fc15bf1083
Abby <abbylijtestin@gmail.com>
2026-06-10 22:21:10 -0300

    program vehiculos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TP_POO_Lijtenstein_Valen.Vehiculos;
+
+Console.WriteLine("===== SISTEMA DE VEHÍCULOS =====");
+
+List<Vehiculo> vehiculos = new List<Vehiculo>();
+
+vehiculos.Add(new Auto("Toyota Corolla"));
+vehiculos.Add(new Moto("Yamaha MT-03"));
+
+foreach (Vehiculo vehiculo in vehiculos)
+{
+    vehiculo.MostrarInfo();
+
+    Console.WriteLine("Acelerando...");
+    vehiculo.Acelerar();
+    vehiculo.MostrarInfo();
+
+    Console.WriteLine("Frenando...");
+    vehiculo.Frenar();
+    vehiculo.MostrarInfo();
+
+    Console.WriteLine("--------------------------------");
+}
